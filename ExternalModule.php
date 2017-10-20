c3dac1b23ed8248126c59bd232dfb84b3cc963de
Dileep <rajputd@ufl.edu>
2017-10-20 16:49:39 -0400

    Add ExternalModule.php skeleton

---
+++ ExternalModule.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Provides ExternalModule class for Multi-DET module.
+ */
+
+namespace SaveSurveyPdfToFieldModule\ExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+/**
+ * ExternalModule class for save_survey_pdf_to_field.
+ */
+class ExternalModule extends AbstractExternalModule {
+  //nothing yet
+}
