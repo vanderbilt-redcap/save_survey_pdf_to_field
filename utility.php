b47857df5349e5cfa9a7c9553ba8361222feb493
Dileep <rajputd@ufl.edu>
2017-10-27 16:32:55 -0400

    Add uploadPdfToEdocs method to utility.php

--- utility.php
+++ utility.php
@@ -3,6 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once APP_PATH_DOCROOT . '../redcap_connect.php';
+require_once APP_PATH_DOCROOT . 'Classes/Files.php';
 
 function makePDF($project_id, $record, $instrument, $event_id, $repeat_instance) {
   $filename = "project" . $project_id .
@@ -17,6 +18,26 @@ function makePDF($project_id, $record, $instrument, $event_id, $repeat_instance)
   return $full_path_to_temp_file;
 }
 
-
+/**
+ * uploads the pdf file at $file_path into the edocs folder and tags its stored name
+ * file name.
+ *
+ * @param string $file_path
+ *  location of the pdf file to be stored into the edocs folder
+ *
+ * @param string $filename
+ *  name of the pdf file to be displayed to users
+ *
+ * @return int $doc_id
+ *  doc_id of the file in the redcap_edocs_metadata table. Returns 0 on failure.
+ */
+function uploadPdfToEdocs($file_path, $filename) {
+  $_FILE['type'] = "application/pdf";
+  $_FILE['name'] = $filename . date('Y-m-d_Hi') . ".pdf";
+  $_FILE['tmp_name'] = $file_path;
+  $_FILE['size'] = filesize($_FILE['tmp_name']);
+  $doc_id = Files::uploadFile($_FILE);
+  return $doc_id;
+}
 
  ?>
